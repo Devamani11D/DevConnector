e04de931437a4004fa9716eb8053d90727debea3
Devamani11D <duddekuntadevamani@gmail.com>
2023-07-03 10:17:47 +0530

    deploy

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import UserRoute from './routes/api/users.js';
 import PostRoute from './routes/api/posts.js';
 import AuthRoute from './routes/api/auth.js';
 import ProfileRoute from './routes/api/profile.js';
+import path from 'path';
 const app=express();
 
 //Database Call
@@ -14,15 +15,25 @@ const port=process.env.PORT || 5500;
 //similar to bodyparser to get req.body
 app.use(express.json({extended:false}));
 app.use(cors());
-app.get('/',(req,res)=>
-{
-    res.send("API running");
-})
+// app.get('/',(req,res)=>
+// {
+//     res.send("API running");
+// })
 
 app.use('/api/users',UserRoute);
 app.use('/api/posts',PostRoute);
 app.use('/api/profile',ProfileRoute);
 app.use('/api/auth',AuthRoute);
+
+
+//serve static assets in production
+if(process.env.NODE_ENV==='production'){
+    app.use(express.static('client/build'));
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    })
+}
+
 app.listen(port,()=>{
     console.log(`App listening at port ${port}`);
 })
